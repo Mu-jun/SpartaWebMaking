60ccf325381afcace597b7a7bfa0b367cea1da3c
unknown <placetobe@naver.com>
2022-02-11 16:52:26 +0900

    차 등록 페이지 만들어봄

--- app.py
+++ app.py
@@ -2,12 +2,35 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-# DB 관련 주석
-# from pymongo import MongoClient
-# client = MongoClient('mongodb://test:test@localhost', 27017)
-# db = client.chacha
+# DB 관련
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbchacha
 
-# feature_WJY: 브랜치 작업은 어떻게 하는 건지 test
+# 차 정보 입력하기(POST) API
+
+@app.route('/save', methods=['POST'])
+def save_tea():
+    name_receive = request.form['name_give']
+    blend_receive = request.form['blend_give']
+    benefit_receive = request.form['benefit_give']
+    caffeine_receive = request.form['caffeine_give']
+    desc_receive = request.form['desc_give']
+
+    doc = {
+        'name': name_receive,
+        'blend': blend_receive,
+        'benefit': benefit_receive,
+        'caffeine': caffeine_receive,
+        'desc' : desc_receive,
+
+    }
+
+    db.tealist.insert_one(doc)
+
+
+    return jsonify({'msg': '차 등록이 완료되었습니다!'})
+
 
 
 @app.route('/')
