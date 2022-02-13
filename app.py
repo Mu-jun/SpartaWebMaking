e0ba568b49d8cd936fc9c8b69cabe6a0155a6cea
sugg <245nha@gmail.com>
2022-02-13 22:26:05 +0900

    가입간단구현

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 
 from flask import Flask, render_template, request, jsonify
+import hashlib
+import jwt
+import datetime
+
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False #한글 깨짐 현상 해결코드
 
 # DB 관련
 from pymongo import MongoClient
-client = MongoClient('mongodb://test:test@54.180.2.121', 27017)
+client = MongoClient('localhost', 27017)
 # client = MongoClient('localhost', 27017)
 db = client.dbchacha
 
@@ -25,8 +30,8 @@ def save_tea():
         'name': name_receive,
         'blend': blend_receive,
         'benefit': benefit_receive,
-        'caffeineOX': caffeineOX_receive,
-        'caffeine': caffeine_receive,
+        'caffeineOX': bool(caffeineOX_receive),
+        'caffeine': int(caffeine_receive),
         'desc': desc_receive,
         'caution': caution_receive,
         'img': img_receive,
@@ -40,12 +45,69 @@ def save_tea():
 def home():
    return render_template('index.html')
 
-#GET요청API코드
+# [회원가입 API]
+# id, pw, nickname을 받아서, mongoDB에 저장합니다.
+# 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장합니다.
+@app.route('/sign/signup', methods=['POST'])
+def api_register():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    nickname_receive = request.form['nickname_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    doc2 = {'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive}
+
+    db.user.insert_one(doc2)
+
+    return jsonify({'result': 'success'})
+
+# [로그인 API]
+# id, pw를 받아서 맞춰보고, 토큰을 만들어 발급합니다.
+@app.route('/sign/log_in', methods=['POST'])
+def api_login():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
+    result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
+
+    # 찾으면 JWT 토큰을 만들어 발급합니다.
+    if result is not None:
+        # JWT 토큰에는, payload와 시크릿키가 필요합니다.
+        # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있습니다.
+        # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
+        # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
+        payload = {
+            'id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        # token을 줍니다.
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+@app.route('/sign')
+def signup_page():
+   return render_template('01_login.html')    
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
+
+"""    #GET요청API코드
 @app.route('/test', methods=['GET'])
 def test_get():
    title_receive = request.args.get('title_give')
    print(title_receive)
-   return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+   return jsonify({'result':'success', 'msg': 'GET!'})
+
+
 #GET확인코드
 # $.ajax({
 #     type: "GET",
@@ -62,6 +124,7 @@ def test_post():
    title_receive = request.form['title_give']
    print(title_receive)
    return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+
 #POST확인코드
 # $.ajax({
 #     type: "POST",
@@ -70,7 +133,4 @@ def test_post():
 #     success: function(response){
 #        console.log(response)
 #     }
-#   })
-
-if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+#   }) """
