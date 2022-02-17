1b8aa74241679fb1943d9a4a2e86b627525d62f5
Mu-jun <suo24691@gmail.com>
2022-02-18 01:08:52 +0900

    refactor & build 로그인 기능 컨포너트화 가상환경 업데이트

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 
+from urllib import response
 from flask import Flask, render_template, request, jsonify, make_response
 from flask_jwt_extended import *
 from flask_bcrypt import *
@@ -21,8 +22,13 @@ app.config['JSON_AS_ASCII'] = False #한글 깨짐 현상 해결코드
 app.config['JWT_SECRET_KEY'] = chachaconfig.jwt_key
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(minutes=5)
 app.config['JWT_REFRESH_TOKEN_EXPIRES'] = datetime.timedelta(days=10)
+app.config['JWT_ACCESS_COOKIE_NAME'] = 'chachaAccessToken'
+app.config['JWT_REFRESH_COOKIE_NAME'] = 'chachaRefreshToken'
+#app.config['JWT_TOKEN_LOCATION'] = ["cookies","headers"]
+
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['SESSION_COOKIE_SECURE'] = True
+
 
 # configure Flask App with JWT support
 # JWT 지원으로 Flask App 구성
@@ -248,7 +254,7 @@ def hash_pass(password, id):
 
     return password
 
-@app.route('/sign/signup_test', methods=['POST'])
+@app.route('/sign/signup', methods=['POST'])
 def signup():
     print('signup start')
     
@@ -276,9 +282,11 @@ def signup():
 @app.route('/sign/signin', methods=['POST'])
 def api_signin():
     print('signin start')
+    response = make_response(render_template('/sign_test.html'))
+    receive = request.get_json();
     
-    id_receive = request.form['id_give'].upper()
-    pass_receive = request.form['pass_give']
+    id_receive = receive['id_give'].upper()
+    pass_receive = receive['pass_give']
     
     print(id_receive)
     
@@ -289,66 +297,35 @@ def api_signin():
     
     if(user):
         if(hashed_password == user['password']):
-            access_token = create_access_token(identity=user['id'])
+            response = jsonify({'success':'환영합니다.'+user['nickname']+'님'})
+            
+            access_token = create_access_token(identity=user['id'])            
+            response.set_cookie('chachaAccessToken', value=access_token, samesite=None, httponly=True)
+            
             refresh_token = create_refresh_token(identity=user['id'])
-        
-            return jsonify({'success':'환영합니다.'+user['nickname']+'님','access_token':access_token, 'refresh_token':refresh_token})
+            response.set_cookie('chachaRefreshToken', value=refresh_token, samesite = None, httponly=True)
+            
+            return response
     else:
         return jsonify({'fail':'ID와 비밀번호를 확인해주세요.'})
     
 #cookie 토큰관리
 
-@app.route('/set_access_token', methods=['GET', 'POST'])
+@app.route('/unset_token', methods=['GET'])
 def set_access_token():
-    print('set_access_token start')    
+    print('unset_token start')    
     response = make_response(render_template('/sign_test.html'))
-    
-    if request.method == "POST":
         
-        user_id = request.form['id_give']
-    
-        if(user_id):
-            print('액세스토큰쿠키 생성')
-            access_token = create_access_token(identity=user_id)
-            
-            response.set_cookie('chachaAccessToken', value=access_token, samesite=None, httponly=True) #path='/localhost', domain='/localhost', httponly=True
-        else:
-            print('액세스토큰쿠키 삭제2')
-            response.delete_cookie('chachaAccessToken')
-    else:
-        print('액세스토큰쿠키 삭제1')
-        response.delete_cookie('chachaAccessToken')
+    response.delete_cookie('chachaAccessToken')
+    response.delete_cookie('chachaRefreshToken')
              
     return response
-        
-@app.route('/set_refresh_token', methods=['GET', 'POST'])
-def set_refresh_token():
-    print('set_refresh_token start')
-    
-    response = make_response(render_template('/sign_test.html'))
-    
-    if request.method == "POST":
-        user_id = request.form['id_give']
-        
-        if(user_id):
-            print('리프레쉬토큰쿠키 생성')
-            refresh_token = create_refresh_token(identity=user_id)            
-            response.set_cookie('chachaRefreshToken', value=refresh_token, samesite = None, httponly=True)#path='/localhost', domain='/localhost', 
-        else:
-            print('리프레쉬토큰쿠키 삭제2')
-            response.delete_cookie('chachaRefreshToken')
-    else:
-        print('리프레쉬토큰쿠키 삭제1')
-        response.delete_cookie('chachaRefreshToken')
-        
-    return response
-
+  
 @app.route('/get_access_token', methods=['GET'])
 def api_get_access_token():
     print('get_access_token start')
     
-    result = request.cookies.get('chachaAccessToken')
-    print(result)
+    result = request.cookies.get('chachaAccessToken')    
     
     if result is not None:
         return jsonify(result)
@@ -360,8 +337,11 @@ def api_get_refresh_token():
     print('get_refresh_token start')
     
     result = request.cookies.get('chachaRefreshToken')
-    
-    return result
+    print(result)
+    if result is not None:
+        return jsonify(result)
+    else:
+        return jsonify(None)
     
 @app.route('/refresh', methods=['GET'])
 @jwt_required(refresh=True)
@@ -377,12 +357,15 @@ def refresh():
 @jwt_required()
 def api_change_pass():
     print('change_pass start')
-    current_user = get_jwt_identity().upper()
-        
+    current_user = get_jwt_identity().upper()        
     print(current_user)
     
-    pass_receive = request.form['pass_give']
-    new_password = request.form['new_pass_give']
+    print(request.get_data())
+    receive = request.get_json()
+    print(receive)
+    
+    pass_receive = receive['pass_give']
+    new_password = receive['new_pass_give']
     
     hashed_password = hash_pass(pass_receive,current_user)
     new_password = hash_pass(new_password,current_user)
