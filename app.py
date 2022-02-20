fd3abcaa2a6968616b995cbeeff1e2c5dbc6b02f
alpha-fly <99331753+alpha-fly@users.noreply.github.com>
2022-02-20 19:09:52 +0900

    DB 새로운 주소로 주소만 변경
    client = MongoClient('mongodb://test:test@52.78.104.136', 27017)

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ from pymongo import MongoClient, ReturnDocument
 # **********************************************************
 
 client = MongoClient('localhost', 27017)
-# client = MongoClient('mongodb://test:test@54.180.2.121', 27017)
+# client = MongoClient('mongodb://test:test@52.78.104.136', 27017)
 
 db = client.dbchacha
 
